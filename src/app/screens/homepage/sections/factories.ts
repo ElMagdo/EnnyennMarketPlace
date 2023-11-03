ac10239d0796652d505c22cb0d1527257f118b96
ElMagdo <magdoe19@gmail.com>
2023-11-03 23:14:45 +0100

    finished styling factories section

--- src/app/screens/homepage/sections/factories.ts
+++ src/app/screens/homepage/sections/factories.ts
@@ -8,25 +8,37 @@ import { Component } from '@angular/core';
       <div class="cap">Source Solar Directly<br>from Factories</div>
       <div class="cnt">
         <div class="lft">
-          <div class="qb">
+        <div><img src="../../assets/homepage/fac1.png" alt="fac-1"/></div>
+          <div class="btm qb">
             <div class="tt">Qqan Solar<br>Manufacturers Co. LTD</div>
-            <div class="btx"><span>icon</span> Guangzhou, China</div>
-            <div class="tx">Take a tour <span>icon</span></div>
+            <div class="btx">
+              <span class="geo"><img src="../../assets/homepage/geo-alt.png" alt="geo-alt"/></span> 
+              &nbsp;Guangzhou, China
+            </div>
+            <div class="tx">Take a tour <span class="chv"><img src="../../assets/homepage/chevron-right.png" alt="chevron-right"/></span></div>
           </div>
         </div>
         <div class="ryt">
         <div class="s">
-          <div class="sb">
+          <div><img src="../../assets/homepage/fac2.png" alt="fac-2"/></div>
+          <div class="btm sb">
             <div class="tt">Shushan & Co. LTD</div>
-            <div class="btx"><span>icon</span> Frankfurt, Germany</div>
-            <div class="tx">Take a tour <span>icon</span></div>
+            <div class="btx">
+              <span class="geo"><img src="../../assets/homepage/geo-alt.png" alt="geo-alt"/></span>
+              &nbsp;Frankfurt, Germany
+            </div>
+            <div class="tx">Take a tour <span class="chv"><img src="../../assets/homepage/chevron-right.png" alt="chevron-right"/></span></div>
           </div>
         </div>
         <div class="b">
-          <div class="bb">
+          <div><img src="../../assets/homepage/fac3.png" alt="fac-3"/></div>
+          <div class="btm bb">
             <div class="tt">BWW Solar Suppliers GmBH</div>
-            <div class="btx"><span>icon</span> Frankfurt, Germany</div>
-            <div class="tx">Take a tour <span>icon</span></div>
+            <div class="btx">
+              <span class="geo"><img src="../../assets/homepage/geo-alt.png" alt="geo-alt"/></span> 
+              &nbsp;Frankfurt, Germany
+            </div>
+            <div class="tx">Take a tour <span class="chv"><img src="../../assets/homepage/chevron-right.png" alt="chevron-right"/></span></div>
           </div>
         </div>
         </div>
@@ -46,26 +58,45 @@ import { Component } from '@angular/core';
     font-size: 34px;
     font-weight: bold;
     color: #3f6632;
-    margin-bottom: 15px;
+    margin-bottom: 30px;
   }
   .cnt {
     display: flex;
     color: #ffffff;
   }
   .lft {
-    background-image: url("src/assets/homepage/fac1.png");
-    background-color: #ededed;
+    margin-right: 25px;
+  }
+  .lft, .s, .b {
+    position: relative;
+  }
+  .s {
+    margin-bottom: 20px;
+  }
+  .qb, .sb, .bb {
+    position: absolute;
+    bottom: 0;
+    padding: 20px 30px;
+    background-color: #00000080;
+    width: 100%;
+    border-radius: 0 0 15px 15px;
   }
   .tt {
     font-size: 20px;
     font-weight: bold;
+    margin-bottom: 15px;
   }
   .btx {
     font-size: 13px;
     font-weight: semi-bold;
+    margin-bottom: 25px;
   }
   .tx {
     font-size: 13px;
+  }
+  .btx, .tx {
+    display: flex;
+    align-items: center;
   }
   `]
 })
